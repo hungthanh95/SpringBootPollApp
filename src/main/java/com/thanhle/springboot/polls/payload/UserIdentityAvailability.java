e3955d915435e04e7fb7fad1f87ee461bb7efa27
hungthanh95 <hungthanh95@gmail.com>
2020-12-29 00:33:54 +0700

    add poll choice and vote

---
+++ src/main/java/com/thanhle/springboot/polls/payload/UserIdentityAvailability.java
@@ -0,0 +1,13 @@
+package com.thanhle.springboot.polls.payload;
+
+public class UserIdentityAvailability {
+    private Boolean available;
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+}
